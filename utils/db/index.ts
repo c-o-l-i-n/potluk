b637878d42a7e7a2483a2729234180490c410303
Colin Williams <colin@colinaw.com>
2022-04-21 23:41:35 -0400

    🔥 Use environment variables for Firebase credentials

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,11 +1,20 @@
-import admin, { ServiceAccount } from 'firebase-admin'
-import serviceAccount from './serviceAccountKey.json'
+import admin from 'firebase-admin'
 
 if (!admin.apps.length) {
 	try {
+		console.log(process.env.FIREBASE_CREDENTIALS_WITHOUT_API_KEY)
+		console.log(process.env.API_KEY)
+
+		const firebaseCredentials = {
+			...JSON.parse(process.env.FIREBASE_CREDENTIALS_WITHOUT_API_KEY || ''),
+			privateKey: process.env.API_KEY?.replace(/\\n/g, '\n'),
+		}
+
+		console.log(firebaseCredentials)
+
 		admin.initializeApp({
-			credential: admin.credential.cert(serviceAccount as ServiceAccount),
-			databaseURL: 'https://potluk-app-default-rtdb.firebaseio.com',
+			credential: admin.credential.cert(firebaseCredentials),
+			databaseURL: `https://${process.env.DATABASE_NAME}.firebaseio.com`,
 		})
 	} catch (error: any) {
 		console.log('Firebase admin initialization error', error.toString())
