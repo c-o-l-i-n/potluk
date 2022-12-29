52311f9ad6c187e6e4d400ffd4e6a85c83bbffd5
Colin Williams <colin@colinaw.com>
2022-12-28 20:50:50 -0500

    🌐 Limit 404 forward to before "/" to avoid infinite requests

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,24 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
 
-const NotFound = () => {
+/**
+ * This page is a hack to achieve the "illusion" of dynamic routing in a static
+ * site hosted on Github Pages.
+ * 
+ * Since Potluk IDs are not in the list of pre-built static routes, Github
+ * Pages shows the 404.html page when a user navigates to a Potluk page,
+ * which loads this component.
+ * 
+ * This component then pushes the user to the normal Potluk page using the
+ * Next.js router. It works becuase Next.js router.push() uses an all-JS SPA
+ * approach to change pages rather than making a new HTTP request.
+ */
+const ForwardToPotluk = () => {
 	const router = useRouter()
 
   useEffect(() => {
-    console.log(404);
-    router.push(router.asPath)
+    router.push(router.asPath.split('/')[1])
   }, [])
 }
 
-export default NotFound
+export default ForwardToPotluk
