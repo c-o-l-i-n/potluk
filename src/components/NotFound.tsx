f33f8f4f1397a0f38a945ae1d3399c58950b9f0e
Colin Williams <colin@colinaw.com>
2023-02-05 17:39:59 -0500

    🌐 Enhance 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import React, { ReactElement } from 'react'
+import Image from 'next/image'
 
 interface Props {
   potlukId: string
@@ -12,8 +14,18 @@ export default function NotFound ({ potlukId }: Props): ReactElement {
         <title>404 - Potluk Not Found</title>
       </Head>
       <center>
-        <h1>404</h1>
+        <Image
+          src='/images/404.svg'
+          alt='Page Not Found'
+          width={300}
+          height={200}
+          priority
+        />
         <p>Potluk with ID &quot;{potlukId}&quot; not found.</p>
+        <p><strong>Tip:</strong> Make sure the full link was copied exactly right.</p>
+        <Link href='/' className='button is-primary has-text-weight-bold mt-5'>
+          Home
+        </Link>
       </center>
     </>
   )
